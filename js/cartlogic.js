eec96520da18e91e62d07b0c0d122a0e821b05ec
yasser ramadan <y.r.a.elnobe@gmail.com>
2024-05-29 01:01:07 +0300

    lunch 1.0

--- js/cartlogic.js
+++ js/cartlogic.js
@@ -1,11 +1,84 @@
 
-
+// $('#table-rows').hide()
 var cart = [];
+
+function setCartIocal() {
+    var corner = JSON.stringify(cart)
+    localStorage.setItem('myCart', corner)
+}
 
 function getCart() {
     var data = localStorage.getItem('myCart')
-    data = JSON.parse(data)
-    console.log('data', data);
+    cart = JSON.parse(data)
+    console.log('data', cart);
+    display()
 }
 
 getCart()
+
+function display() {
+    let container = ``
+    let total = 0;
+    cart.map((product, index) => {
+        total += product.productPrice * product.productCount;
+        container += `
+            <tr>
+                <td>${index + 1}</td>
+                <td>${product.productName}</td>
+                <td>${product.productPrice}</td>
+                <td><button type="button" class="btn btn-success" onclick='count(1,${index})'>&lt;</button>
+                </td>
+                <td>
+                <div class="d-inline-block px-1">${product.productCount}
+                </div>
+                </td>
+                <td>
+                    <button type="button" class="btn btn-secondary" onclick='count(-1,${index})'>&gt;</button>
+                </td>
+                <td>${product.productPrice * product.productCount}</td>
+
+                <td><button type="button" class="btn btn-danger" onclick='count("#",${index})'>-</button></td>
+            </tr>`;
+    });
+    $('#table-rows').html(container);
+    $('#totalAll').text(total);
+
+}
+
+function count(x, i) {
+    if (x == 1) {
+        cart[i].productCount++
+    } else if (x == -1) {
+        cart[i].productCount--
+    } else if (x == "#") {
+        cart[i].productCount = 0
+    } else {
+        alert('callback custmer servies')
+    }
+
+    if (cart[i].productCount == 0) {
+        cart.splice(i, 1)
+    }
+    display()
+    setCartIocal()
+}
+
+
+function deleteAll() {
+    cart = []
+    display()
+    setCartIocal()
+    window.location.href = '/index.html'
+}
+
+function sendWhatsapp() {
+    let massage = `اود شراء:
+    `
+    for (const item of cart) {
+        massage += `عدد (${item.productCount})
+        من ${item.productName}  
+        `
+    }
+    console.log(massage);
+    window.location.href = `https://wa.me/+201554367882?text=${massage}`
+}
--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 var products = [
     {
         productId: 1,
-        productName: 'book1',
+        productName: 'الاربعون',
         productDesc: 'lorem',
         productPrice: 50,
         productImg: 'lorem 1',
@@ -50,39 +50,24 @@ var products = [
         productImg: 'lorem 1',
     },
 ];
-
 var cart = [];
 
+localStorage.getItem('myCart') ? cart = JSON.parse(localStorage.getItem('myCart')) : cart = [];
+
 function display() {
-    console.log(products);
-    var container = ``
-    var x = 1
-    // for (product of products) {
-
-    //     container += `<div class="col-6 col-sm-4 col-md-3 col-lg-2 text-center">
-    //     <img src="img/1.png" alt="" class="w-100">
-    //     <h3>${product.productName}</h3>
-    //     <p class="m-0 p-0">${product.productDesc}</p>
-    //     <div class="btn btn-group w-100">
-    //         <button type="button" class="btn btn-info" onclick = 'addToCard(product)'>add </button>
-    //         <button type="button" class="btn btn-success">Detailes </button>
-    //     </div>
-    // </div>`;
-    // }
-
-    for (var i = 0; i < products.length; i++) {
-
-        container += `<div class="col-6 col-sm-4 col-md-3 col-lg-2 text-center">
-        <img src="img/1.png" alt="" class="w-100">
-        <h3>${products[i].productName}</h3>
-        <p class="m-0 p-0">${products[i].productDesc}</p>
-        <div class="btn btn-group w-100">
-            <button type="button" class="btn btn-info" onclick = 'addToCard()'>add </button>
-            <button type="button" class="btn btn-success" onclick = 'addToCart(${products[i].productId})'>Detailes </button>
-        </div>
-    </div>`;
-    }
-    $('#products').html(container);
+    products.map((product, index) => {
+            let container = `<div class="col-6 col-sm-4 col-md-3 col-lg-2 text-center">
+                <img src="img/1.png" alt="" class="w-100">
+                <h3>${product.productName}</h3>
+                <p class="m-0 p-0">${product.productDesc}</p>
+                <div class="btn btn-group w-100">
+                    <button type="button" class="btn btn-info" onclick = 'addToCard(${index})'>add </button>
+                    <button type="button" class="btn btn-success" onclick = 'addToCart(${index})'>Detailes </button>
+                </div>
+            </div>`;
+            $('#products').append(container);
+        }
+    );
 }
 
 display()
@@ -90,56 +75,45 @@ display()
 function setCartIocal() {
     var corner = JSON.stringify(cart)
     localStorage.setItem('myCart', corner)
-    getCart()
 }
 
-function getCart() {
-    var data = localStorage.getItem('myCart')
-    data = JSON.parse(data)
-    console.log('data', data);
-}
-
-
-// function addToCard(opj) {
-
-//     console.log('addToCard', cart);
-//     var x = false
-//     for (let i = 0; i < cart.length; i++) {
-//         if (opj.productId == cart[i].productId) {
-//             cart[i].count++
-//             x = true
-//             break;
-//         }
-//     }
-//     if (x == false) {
-//         opj.count = 1
-//         cart.push(opj)
-//     }
-//     console.log('x', x);
-//     console.log('cart', cart);
-//     console.log('opj', opj);
-
-// }
-
-//test
-function addToCart(id) {
-    var x = false
+function addToCart(index) {
+    x = false
+    let newproduct = products.slice(index, index + 1)[0];
 
     for (let i = 0; i < cart.length; i++) {
-        if (id == cart[i].productId) {
+        if (newproduct.productId == cart[i].productId) {
             cart[i].productCount++
             x = true
         }
     }
 
     if (x == false) {
-        for (let i = 0; i < products.length; i++) {
-            if (id == products[i].productId) {
-                products[i].productCount = 1
-                cart.push(products[i])
-                break;
-            }
-        }
+        newproduct.productCount = 1
+        cart.push(newproduct)
+        console.log('cart', cart);
     }
+
     setCartIocal()
+
+    // let newproduct = products.slice(index, index + 1);
+    // let search = cart.filter((product) => { product.productId == newproduct[0].productId; 
+    //     console.log('product', product.productId);
+    //     console.log('newproduct' ,newproduct[0].productId);
+    // });
+
+    // if (search.length > 0) {
+    //     console.log("increase");
+
+    // for (let i = 0; i < cart.length; i++) {
+    //     if (id == cart[i].productId) {
+    //         cart[i].productCount++
+    //         x = true
+    //     }
+    // }
+    // } else {
+    //     newproduct[0].productCount = 1;
+    //     cart.push(newproduct[0]);
+    // }
+
 }
